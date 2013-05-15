6018ef116e870edca81aa2927860e5701bcda99b
David Camre <dac@frv.dk>
2013-05-15 11:56:01 +0200

    Modified monalisa panel

--- epd-ship/src/main/java/dk/dma/epd/ship/gui/Panels/MonaLisaCommunicationPanel.java
+++ epd-ship/src/main/java/dk/dma/epd/ship/gui/Panels/MonaLisaCommunicationPanel.java
@@ -21,6 +21,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -31,31 +33,41 @@ import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.JScrollPane;
 
-public class MonaLisaCommunicationPanel extends JPanel {
+public class MonaLisaCommunicationPanel extends JPanel implements
+        ActionListener {
 
     private static final long serialVersionUID = 1L;
-    
-    
+
     JTextArea chatMessages;
     private JButton btnSend;
     private JTextField textField;
     private JPanel panel;
-    private JTextField textField_1;
+    private JTextField txtField;
 
     public MonaLisaCommunicationPanel() {
         super();
-        
-//        setMinimumSize(new Dimension(200, 500));
-        
+
+        // setMinimumSize(new Dimension(200, 500));
+
         setBorder(new LineBorder(Color.GRAY));
         GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[] { 10, 0 };
-        gridBagLayout.rowHeights = new int[] { 119, 10 };
+        gridBagLayout.rowHeights = new int[] { 10, 120, 10 };
         gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-        gridBagLayout.rowWeights = new double[] { 1.0, 1.0 };
+        gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 1.0 };
         setLayout(gridBagLayout);
-        
-        
+
+        JLabel commsTitle = new JLabel("STCC Coms");
+        commsTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        commsTitle.setFont(new Font("Segoe UI", Font.BOLD, 14));
+
+        GridBagConstraints gbc_commsTitle = new GridBagConstraints();
+        gbc_commsTitle.anchor = GridBagConstraints.NORTH;
+        gbc_commsTitle.insets = new Insets(0, 0, 5, 0);
+        gbc_commsTitle.gridx = 0;
+        gbc_commsTitle.gridy = 0;
+        add(commsTitle, gbc_commsTitle);
+
         chatMessages = new JTextArea("");
         chatMessages.setFont(new Font("Monospaced", Font.PLAIN, 12));
         chatMessages.setBackground(new Color(240, 240, 240));
@@ -63,48 +75,66 @@ public class MonaLisaCommunicationPanel extends JPanel {
         chatMessages.setEditable(false);
         chatMessages.setBorder(null);
 
-//        chatMessages.setMinimumSize(new Dimension(200, 500));
-        
-        chatMessages.setText("Hello \n Line \nLine \nLine\nLine");
-        
+        // chatMessages.setMinimumSize(new Dimension(70, 200));
+
+        // chatMessages.setText("Hello \n Line \nLine \nLine\nLine");
+
         JScrollPane scrollPane = new JScrollPane(chatMessages);
-//        scrollPane.setMinimumSize(new Dimension(200, 500));
+        // scrollPane.setMinimumSize(new Dimension(200, 500));
         GridBagConstraints gbc_scrollPane = new GridBagConstraints();
         gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
         gbc_scrollPane.fill = GridBagConstraints.BOTH;
         gbc_scrollPane.gridx = 0;
-        gbc_scrollPane.gridy = 0;
+        gbc_scrollPane.gridy = 1;
         add(scrollPane, gbc_scrollPane);
-        
+
         JPanel panel_1 = new JPanel();
         GridBagConstraints gbc_panel_1 = new GridBagConstraints();
         gbc_panel_1.anchor = GridBagConstraints.SOUTH;
         gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
         gbc_panel_1.gridx = 0;
-        gbc_panel_1.gridy = 1;
+        gbc_panel_1.gridy = 2;
         add(panel_1, gbc_panel_1);
         GridBagLayout gbl_panel_1 = new GridBagLayout();
-        gbl_panel_1.columnWidths = new int[] {0, 2};
-        gbl_panel_1.rowHeights = new int[] {0};
-        gbl_panel_1.columnWeights = new double[]{1.0, 0.0};
-        gbl_panel_1.rowWeights = new double[]{0.0};
+        gbl_panel_1.columnWidths = new int[] { 0, 2 };
+        gbl_panel_1.rowHeights = new int[] { 0 };
+        gbl_panel_1.columnWeights = new double[] { 1.0, 0.0 };
+        gbl_panel_1.rowWeights = new double[] { 0.0 };
         panel_1.setLayout(gbl_panel_1);
-        
-        textField_1 = new JTextField();
-        GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-        gbc_textField_1.insets = new Insets(0, 0, 0, 5);
-        gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-        gbc_textField_1.gridx = 0;
-        gbc_textField_1.gridy = 0;
-        panel_1.add(textField_1, gbc_textField_1);
-        textField_1.setColumns(10);
-        
-        JButton btnNewButton = new JButton("New button");
-        GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-        gbc_btnNewButton.gridx = 1;
-        gbc_btnNewButton.gridy = 0;
-        panel_1.add(btnNewButton, gbc_btnNewButton);
+
+        txtField = new JTextField();
+        GridBagConstraints gbc_txtField = new GridBagConstraints();
+        gbc_txtField.insets = new Insets(0, 0, 0, 5);
+        gbc_txtField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txtField.gridx = 0;
+        gbc_txtField.gridy = 0;
+        panel_1.add(txtField, gbc_txtField);
+        txtField.setColumns(10);
+
+        JButton sendBtn = new JButton("Send");
+        GridBagConstraints gbc_sendBtn = new GridBagConstraints();
+        gbc_sendBtn.gridx = 1;
+        gbc_sendBtn.gridy = 0;
+        panel_1.add(sendBtn, gbc_sendBtn);
+
+        sendBtn.addActionListener(this);
+
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        
+        if (!txtField.getText().equals("")){
+            
+        String msg = txtField.getText() + "\n" ;
+        txtField.setText("");
+        String currentText = chatMessages.getText();
+
+        currentText = currentText  + msg;
+
+        chatMessages.setText(currentText);
+        // chatMessages
+        }
+    }
 
 }
