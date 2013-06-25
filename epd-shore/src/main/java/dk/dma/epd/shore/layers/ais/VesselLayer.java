c27a3b454e3d8575933833400ec1cc12fe92b1b4
David Camre <dac@frv.dk>
2013-06-25 11:21:32 +0200

    Comparison between possible null fixed

--- epd-shore/src/main/java/dk/dma/epd/shore/layers/ais/VesselLayer.java
+++ epd-shore/src/main/java/dk/dma/epd/shore/layers/ais/VesselLayer.java
@@ -87,7 +87,8 @@ public class VesselLayer extends CenterRaster {
      * @param shipType Ship type relative to "GUIDELINES FOR THE INSTALLATION OF A SHIPBORNE AUTOMATIC IDENTIFICATION SYSTEM (AIS)"
      */
     public void setImageIcon(String shipType) {
-        if(!this.shipType.equals(shipType)) {
+        System.out.println("This shiptype: " + this.shipType + " new shipType = " + shipType);
+        if(this.shipType != shipType) {
             this.shipType = shipType;
             if (shipType.startsWith("Passenger")) {
                 vesselIcon = staticImages.getVesselBlue();
