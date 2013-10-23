b2443175db0320f37a4bf4abccdd463c8e9e821c
Janus Varmarken <varmarken@gmail.com>
2013-10-23 14:06:06 +0200

    Changed AisTargetInfoPanel to extend from common version of InfoPanel.

--- epd-ship/src/main/java/dk/dma/epd/ship/layers/ais/AisTargetInfoPanel.java
+++ epd-ship/src/main/java/dk/dma/epd/ship/layers/ais/AisTargetInfoPanel.java
@@ -27,7 +27,7 @@ import dk.dma.epd.common.prototype.ais.VesselTarget.AisClass;
 import dk.dma.epd.common.text.Formatter;
 import dk.dma.epd.common.util.Util;
 import dk.dma.epd.ship.EPDShip;
-import dk.dma.epd.ship.gui.InfoPanel;
+import dk.dma.epd.common.prototype.gui.util.InfoPanel;
 import dk.frv.enav.common.xml.risk.response.Risk;
 import dk.frv.enav.common.xml.risk.response.RiskList;
 
