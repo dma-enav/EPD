09022e64dfe106cbaff8c7f1d41ca72456597bf3
Adam Due Hansen <Adamduehan@gmail.com>
2014-01-24 14:47:03 +0100

    EPD-34: button colors are the same as the others.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/gui/GoBackButton.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/gui/GoBackButton.java
@@ -15,7 +15,18 @@
  */
 package dk.dma.epd.common.prototype.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 
 import dk.dma.epd.common.prototype.event.GoBackMouseListener;
 import dk.dma.epd.common.prototype.event.HistoryListener;
@@ -29,7 +40,7 @@ import dk.dma.epd.common.prototype.gui.views.CommonChartPanel;
  *
  */
 public class GoBackButton extends ButtonLabelCommon {
-
+    
     /****************\
     * private fields *
     \****************/
@@ -37,6 +48,16 @@ public class GoBackButton extends ButtonLabelCommon {
     private HistoryListener historyListener;    // The history listener associated with this button. 
     private CommonChartPanel chartPanel;        // The chart panel which controls projection.
     private GoForwardButton goForwardButton;    // The opposite button of this: the going forward button.
+    
+    public static Font defaultFont = new Font("Arial", Font.PLAIN, 11);
+    public static Color textColor = new Color(237, 237, 237);
+    public static Color clickedColor = new Color(80, 80, 80);
+    public static Color standardColor = new Color(128, 128, 128);
+    public static Color borderColor =  new Color(45, 45, 45);
+    
+    public static Border toolPaddingBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, borderColor);
+    public static Border toolInnerEtchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED,
+            new Color(37, 37, 37), borderColor);
     
     /**
      * Constructs a new button with an arrow.
@@ -51,6 +72,63 @@ public class GoBackButton extends ButtonLabelCommon {
     /****************\
     * public methods *
     \****************/
+    
+    public void styleButton(final JLabel label){
+
+        label.setPreferredSize(new Dimension(80, 25));
+        label.setFont(defaultFont);
+        label.setForeground(textColor);
+        label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1,borderColor));
+        label.setBackground(standardColor);
+        label.setOpaque(true);
+        
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        label.addMouseListener(new MouseAdapter() {  
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (label.isEnabled()){
+                label.setBackground(clickedColor);
+                }
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (label.isEnabled()){
+                    
+                label.setBackground(standardColor);
+                }
+            }
+        });
+    }
+
+    public void styleIconButton(final JLabel label){
+        label.setPreferredSize(new Dimension(40, 25));
+        
+        label.setOpaque(true);
+        label.setBorder(toolPaddingBorder);
+        label.setBackground(standardColor);
+        
+        label.addMouseListener(new MouseAdapter() {  
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (label.isEnabled()){
+                    label.setBackground(clickedColor);
+                    label.setBorder(BorderFactory.createCompoundBorder(toolPaddingBorder, toolInnerEtchedBorder));
+                    label.setOpaque(true);
+                }
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (label.isEnabled()){
+                    
+                    label.setBorder(toolPaddingBorder);
+                    label.setBackground(standardColor);
+                }
+            }
+        });
+    }
     
     /**
      * Adds a HistoryListener to the button.
--- epd-common/src/main/java/dk/dma/epd/common/prototype/gui/GoForwardButton.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/gui/GoForwardButton.java
@@ -15,7 +15,18 @@
  */
 package dk.dma.epd.common.prototype.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 
 import dk.dma.epd.common.prototype.event.GoForwardMouseListener;
 import dk.dma.epd.common.prototype.event.HistoryListener;
@@ -37,6 +48,16 @@ public class GoForwardButton extends ButtonLabelCommon {
     private HistoryListener historyListener;
     private CommonChartPanel chartPanel;
     private GoBackButton goBackButton;
+    
+    public static Font defaultFont = new Font("Arial", Font.PLAIN, 11);
+    public static Color textColor = new Color(237, 237, 237);
+    public static Color clickedColor = new Color(80, 80, 80);
+    public static Color standardColor = new Color(128, 128, 128);
+    public static Color borderColor =  new Color(45, 45, 45);
+    
+    public static Border toolPaddingBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, borderColor);
+    public static Border toolInnerEtchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED,
+            new Color(37, 37, 37), borderColor);
 
     /**
      * Constructs a new button with an arrow.
@@ -52,6 +73,63 @@ public class GoForwardButton extends ButtonLabelCommon {
     /****************\
     * public methods *
     \****************/
+    
+    public void styleButton(final JLabel label){
+
+        label.setPreferredSize(new Dimension(80, 25));
+        label.setFont(defaultFont);
+        label.setForeground(textColor);
+        label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1,borderColor));
+        label.setBackground(standardColor);
+        label.setOpaque(true);
+        
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        label.addMouseListener(new MouseAdapter() {  
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (label.isEnabled()){
+                label.setBackground(clickedColor);
+                }
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (label.isEnabled()){
+                    
+                label.setBackground(standardColor);
+                }
+            }
+        });
+    }
+
+    public void styleIconButton(final JLabel label){
+        label.setPreferredSize(new Dimension(40, 25));
+        
+        label.setOpaque(true);
+        label.setBorder(toolPaddingBorder);
+        label.setBackground(standardColor);
+        
+        label.addMouseListener(new MouseAdapter() {  
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (label.isEnabled()){
+                    label.setBackground(clickedColor);
+                    label.setBorder(BorderFactory.createCompoundBorder(toolPaddingBorder, toolInnerEtchedBorder));
+                    label.setOpaque(true);
+                }
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (label.isEnabled()){
+                    
+                    label.setBorder(toolPaddingBorder);
+                    label.setBackground(standardColor);
+                }
+            }
+        });
+    }
     
     /**
      * Adds a HistoryListener to the button.
