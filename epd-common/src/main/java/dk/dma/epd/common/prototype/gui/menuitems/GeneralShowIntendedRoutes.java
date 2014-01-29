146fe81e5df4534a75277e861f371776b2e8a206
peder280370 <peder@carolus.dk>
2014-01-29 09:28:13 +0100

    ShowIntendedRoutes migrated to epd-common

--- epd-common/src/main/java/dk/dma/epd/common/prototype/gui/menuitems/GeneralShowIntendedRoutes.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/gui/menuitems/GeneralShowIntendedRoutes.java
@@ -13,21 +13,19 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-package dk.dma.epd.shore.gui.views.menuitems;
+package dk.dma.epd.common.prototype.gui.menuitems;
 
 import javax.swing.JMenuItem;
 
+import dk.dma.epd.common.prototype.ais.AisHandlerCommon;
 import dk.dma.epd.common.prototype.gui.menuitems.event.IMapMenuAction;
-import dk.dma.epd.shore.ais.AisHandler;
 
 
 public class GeneralShowIntendedRoutes extends JMenuItem implements IMapMenuAction {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
-    private AisHandler aisHandler;
+    
+    private AisHandlerCommon aisHandler;
 
     public GeneralShowIntendedRoutes(String text) {
         super();
@@ -39,7 +37,7 @@ public class GeneralShowIntendedRoutes extends JMenuItem implements IMapMenuActi
         aisHandler.showAllIntendedRoutes();
     }
 
-    public void setAisHandler(AisHandler aisHandler) {
+    public void setAisHandler(AisHandlerCommon aisHandler) {
         this.aisHandler = aisHandler;
     }
 
