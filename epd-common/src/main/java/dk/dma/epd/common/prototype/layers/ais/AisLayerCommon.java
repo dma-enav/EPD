043271ae966754351859bfbd3dca793bbad2f3f0
Janus Varmarken <varmarken@gmail.com>
2014-02-03 13:24:17 +0100

    EPD-61: Added AIS target map to AisLayerCommon. Implemented methods that subclasses can use to modify the map. These methods make sure that the map remains in sync with the graphics list from EPDLayerCommon.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/layers/ais/AisLayerCommon.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/layers/ais/AisLayerCommon.java
@@ -15,6 +15,9 @@
  */
 package dk.dma.epd.common.prototype.layers.ais;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import dk.dma.epd.common.graphics.ISelectableGraphic;
 import dk.dma.epd.common.prototype.ais.AisHandlerCommon;
 import dk.dma.epd.common.prototype.ais.IAisTargetListener;
@@ -31,6 +34,11 @@ public abstract class AisLayerCommon<AISHANDLER extends AisHandlerCommon>
      * The AIS handler that provides AIS data for this layer.
      */
     protected volatile AISHANDLER aisHandler;
+
+    /**
+     * Maps an MMSI to the object handling its graphical representation.
+     */
+    private Map<Long, TargetGraphic> targets = new ConcurrentHashMap<>();
 
     /**
      * The graphic that is currently selected by the user.
@@ -98,5 +106,65 @@ public abstract class AisLayerCommon<AISHANDLER extends AisHandlerCommon>
         if (repaint) {
             this.doPrepare();
         }
+    }
+
+    /**
+     * Add a graphical representation of an AIS target to this layers set of
+     * graphics.
+     * 
+     * @param mmsi
+     *            The MMSI of the AIS target that {@code tg} represents.
+     * @param tg
+     *            Object that manages the graphical representation of the given
+     *            {@code mmsi}.
+     */
+    protected void addTargetGraphic(Long mmsi, TargetGraphic tg) {
+        if (tg == null || mmsi == null) {
+            return;
+        }
+        // Add the new graphic to the map of ais targets
+        TargetGraphic oldVal = this.targets.put(mmsi, tg);
+        synchronized (this.graphics) {
+            if (oldVal != null) {
+                // remove the old graphical representation of this MMSI
+                this.graphics.remove(oldVal);
+            }
+            // add the new graphical representation of this MMSI to the list of
+            // graphics to be displayed.
+            this.graphics.add(tg);
+        }
+    }
+
+    /**
+     * Remove a graphical representation of an AIS target from this layers set
+     * of graphics.
+     * 
+     * @param mmsi
+     *            The MMSI of the AIS target that should have its graphical
+     *            representation removed.
+     */
+    protected void removeTargetGraphic(Long mmsi) {
+        TargetGraphic deleted = this.targets.remove(mmsi);
+        if (deleted != null) {
+            // Successfully removed a graphic object from map
+            // This graphic object should also be present in the list of
+            // graphics to be displayed, hence we need to remove it from the
+            // graphics list.
+            synchronized (this.graphics) {
+                this.graphics.remove(deleted);
+            }
+        }
+    }
+
+    /**
+     * Get the graphical representation of an AIS target with the given MMSI.
+     * 
+     * @param mmsi
+     *            The MMSI to find the graphical representation for.
+     * @return The graphical representation of the MMSI or null if there is
+     *         currently no graphical representation for this MMSI.
+     */
+    protected TargetGraphic getTargetGraphic(Long mmsi) {
+        return mmsi == null ? null : this.targets.get(mmsi);
     }
 }
