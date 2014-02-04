069497076d899b11e09a10e61c803d9ed687361b
Adam Due Hansen <Adamduehan@gmail.com>
2014-02-04 13:36:25 +0100

    Fixed checkstyle

--- epd-common/src/main/java/dk/dma/epd/common/prototype/layers/wms/SingleWMSService.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/layers/wms/SingleWMSService.java
@@ -122,7 +122,7 @@ public final class SingleWMSService extends AbstractWMSService implements ImageS
             // the color we are looking for... Alpha bits are set to opaque
             public int markerRGB = color.getRGB() | 0xFF000000;
 
-            public final int filterRGB(int x, int y, int rgb) {
+            public int filterRGB(int x, int y, int rgb) {
                 if ((rgb | 0xFF000000) == markerRGB) {
                     // Mark the alpha bits as zero - transparent
                     return 0x00FFFFFF & rgb;
