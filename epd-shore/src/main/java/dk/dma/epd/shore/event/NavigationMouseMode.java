0c45ecf7ff9ed341df7bc5a8af265fa66a180dc7
Adam Due Hansen <Adamduehan@gmail.com>
2014-02-18 08:57:13 +0100

    Implemented free navigation zoom when CTRL is down in Shore

--- epd-shore/src/main/java/dk/dma/epd/shore/event/NavigationMouseMode.java
+++ epd-shore/src/main/java/dk/dma/epd/shore/event/NavigationMouseMode.java
@@ -24,6 +24,7 @@ import javax.swing.JPanel;
 
 import com.bbn.openmap.MapBean;
 import com.bbn.openmap.proj.Proj;
+import com.bbn.openmap.proj.ProjMath;
 import com.bbn.openmap.proj.Projection;
 import com.bbn.openmap.proj.coords.LatLonPoint;
 
@@ -56,6 +57,8 @@ public class NavigationMouseMode extends AbstractCoordMouseMode {
     Cursor navCursorMouseClicked;
     Cursor navCursor;
     private ChartPanel chartPanel;
+
+    private boolean ctrlWasUsed;
 
 
     /**
@@ -237,13 +240,16 @@ public class NavigationMouseMode extends AbstractCoordMouseMode {
                 // point2 = e.getPoint();
 
                 if (!e.isControlDown()){
-                point2 = getRatioPoint((MapBean) e.getSource(), point1, e.getPoint());
+                    point2 = getRatioPoint((MapBean) e.getSource(), point1, e.getPoint());
+                    this.ctrlWasUsed = false;
                 }
                 else{
                     point2 = e.getPoint();
+                    this.ctrlWasUsed = true;
                 }
 
                 paintRectangle((MapBean) e.getSource(), point1, point2);
+                
             }
         }
     }
@@ -345,6 +351,73 @@ public class NavigationMouseMode extends AbstractCoordMouseMode {
         Proj p = (Proj) projection;
 
         synchronized (this) {
+            
+            // If control was held down when the mouse was release, resize to fit the points.
+            if (ctrlWasUsed) {
+                
+                Point fakeRatioPoint = null;
+                Point offsetPoint = null;
+                double boxWidth = 0;
+                double boxHeight = 0;
+                double ratio = 0;
+                
+                // If the selected area is more wider than higher.
+                if ( (Math.abs(e.getPoint().x - this.point1.x)) > Math.abs((e.getPoint().y - this.point1.y) )) {
+                                        
+                    // Calculate the ratio with the help of the frame and the selected area.
+                    double frameWidth = this.chartPanel.getMap().getWidth();
+                    boxWidth          = e.getPoint().x - this.point1.x;
+                    ratio             = frameWidth / boxWidth;
+                    
+                    // Calculate the hight of the box, if it had been a ratio point.
+                    boxHeight = this.chartPanel.getMap().getHeight() / ratio;
+                    
+                    // Create a fake ratio point.
+                    fakeRatioPoint = this.getRatioPoint(
+                            (MapBean) e.getSource(), this.point1, new Point(e.getPoint().x, (int) (this.point1.y+boxHeight)));
+                                            
+                    offsetPoint = new Point((int) (this.point1.x - boxWidth/this.point2.x), (int) (e.getPoint().y - boxHeight/2));
+                    
+                // If the selected area is more higher than wider.
+                } else if ( Math.abs((e.getPoint().x - this.point1.x)) < Math.abs((e.getPoint().y - this.point1.y)) ) {
+                                        
+                    // Calculate the ratio with the help of the frame and the selected area.
+                    double frameHeight = this.chartPanel.getMap().getHeight();
+                    boxHeight          = e.getPoint().y - this.point1.y;
+                    ratio              = frameHeight / boxHeight;
+                    
+                    // Calculate the width of the box, if had it been a ratio point.
+                    boxWidth = this.chartPanel.getMap().getWidth() / ratio;
+                    
+                    fakeRatioPoint = this.getRatioPoint(
+                            (MapBean) e.getSource(), this.point1, new Point((int) (this.point1.x+boxWidth), e.getPoint().y));
+
+                    offsetPoint = new Point((int) (this.point1.x - boxWidth / 2), (int) (e.getPoint().y - boxHeight));
+                }
+                    
+                int centerX = (int) (offsetPoint.x + boxWidth / 2);
+                int centerY = (int) (offsetPoint.y + boxHeight / 2);
+                
+                LatLonPoint center = projection.inverse(centerX, centerY);
+                
+                float newScale;
+                if (this.point1.x < e.getPoint().x) {
+                    newScale = ProjMath.getScale(offsetPoint, fakeRatioPoint, projection);                    
+                } else {
+                    newScale = ProjMath.getScale(fakeRatioPoint, offsetPoint, projection);
+                }
+                
+                p.setScale(newScale);
+                p.setCenter(center);
+                map.setProjection(p);
+                this.chartPanel.forceAisLayerUpdate();
+                
+                // Reset points.
+                this.point1 = null;
+                this.point2 = null;
+                
+                return;
+            }
 
             point2 = getRatioPoint((MapBean) e.getSource(), point1, e.getPoint());
 
@@ -363,9 +436,9 @@ public class NavigationMouseMode extends AbstractCoordMouseMode {
                 return;
             }
 
-
             // Figure out the new scale
             float newScale;
+            
             if (point1.x < point2.x) {
                 newScale = com.bbn.openmap.proj.ProjMath.getScale(point1, point2, projection);
             } else {
@@ -377,17 +450,6 @@ public class NavigationMouseMode extends AbstractCoordMouseMode {
             int centery = Math.min(point1.y, point2.y) + dy / 2;
             LatLonPoint center = projection.inverse(centerx, centery);
 
-            // Fire events on main map to change view to match rect1
-            // Debug.output("point1: " +point1);
-            // Debug.output("point2: " +point2);
-            // Debug.output("Centerx: " +centerx +
-            // " Centery: " + centery);
-            // Debug.output("New Scale: " + newScale);
-            // Debug.output("New Center: " +center);
-
-            // Set the parameters of the projection and then set
-            // the projection of the map. This way we save having
-            // the MapBean fire two ProjectionEvents.
             if (newScale < maxScale) {
                 newScale = maxScale;
             }
