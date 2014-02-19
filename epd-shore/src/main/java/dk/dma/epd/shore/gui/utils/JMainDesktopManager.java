1a544901ac4da5a9441a92efda77389fb1d7d428
David Camre <dac@frv.dk>
2014-02-19 13:02:35 +0100

    Fixed checkstyle violations

--- epd-shore/src/main/java/dk/dma/epd/shore/gui/utils/JMainDesktopManager.java
+++ epd-shore/src/main/java/dk/dma/epd/shore/gui/utils/JMainDesktopManager.java
@@ -9,9 +9,9 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.dma.epd.shore.gui.utils;
 
