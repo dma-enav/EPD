0a7e0d53d11b81b0ff9d19a07d13124271aea6af
Janus Varmarken <varmarken@gmail.com>
2014-03-24 14:56:31 +0100

    EPD-190: added IIntendedRouteLayerCommonSettingsObserver and implemented IntendedRouteLayerCommonSettings

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/IntendedRouteLayerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/IntendedRouteLayerCommonSettings.java
@@ -15,12 +15,91 @@
  */
 package dk.dma.epd.common.prototype.settings.layers;
 
+import java.util.Properties;
+
+import com.bbn.openmap.util.PropUtils;
+
 /**
  * @author Janus Varmarken
  */
-public class IntendedRouteLayerCommonSettings extends LayerSettings<OBSERVER> {
+public class IntendedRouteLayerCommonSettings<OBSERVER extends IIntendedRouteLayerCommonSettingsObserver>
+        extends LayerSettings<OBSERVER> {
+
     /**
-     * Use intended route filter.
+     * Key used in the properties file for the setting specifying if intended
+     * route filter is enabled (see {@link #filter}).
+     */
+    private static final String KEY_USE_INTENDED_ROUTE_FILTER = "useIntendedRouteFilter";
+
+    /**
+     * Setting specifying whether to use intended route filter.
      */
     private boolean filter;
+
+    /**
+     * Get if intended route filter is enabled.
+     * 
+     * @return {@code true} if intended route filter is enabled, {@code false}
+     *         if it is disabled.
+     */
+    public boolean isIntendedRouteFilterInUse() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.filter;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Toggles intended route filtering on/off.
+     * 
+     * @param isFilterInUse
+     *            {@code true} if intended route filter should be enabled,
+     *            {@code false} if it should be disabled.
+     */
+    public void setIntendedRouteFilterInUse(final boolean isFilterInUse) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.filter == isFilterInUse) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify.
+            this.filter = isFilterInUse;
+            for (OBSERVER obs : this.observers) {
+                obs.isIntendedRouteFilterInUseChanged(isFilterInUse);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onLoadSuccess(Properties settings) {
+        this.settingLock.writeLock().lock();
+        // Allow super class to initialize its settings fields.
+        super.onLoadSuccess(settings);
+        // Read if intended route filter should be in use.
+        this.setIntendedRouteFilterInUse(PropUtils.booleanFromProperties(
+                settings, KEY_USE_INTENDED_ROUTE_FILTER, this.filter));
+        this.settingLock.writeLock().unlock();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Properties onSaveSettings() {
+        this.settingLock.readLock().lock();
+        Properties toSave = super.onSaveSettings();
+        // Store if intended route filter is enabled.
+        toSave.setProperty(KEY_USE_INTENDED_ROUTE_FILTER,
+                Boolean.toString(this.filter));
+        this.settingLock.readLock().unlock();
+        return toSave;
+    }
 }
