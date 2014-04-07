695346b1bd985cd8e1c7fdddd387eedbf07f77ca
Janus Varmarken <varmarken@gmail.com>
2014-04-07 19:38:15 +0200

    EPD-190: Implemented WMSLayerCommonSettings.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/WMSLayerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/WMSLayerCommonSettings.java
@@ -16,16 +16,100 @@
 package dk.dma.epd.common.prototype.settings.layers;
 
 /**
+ * This class is used to maintain settings for a WMS layer.
+ * 
  * @author Janus Varmarken
  */
-public class WMSLayerCommonSettings extends LayerSettings<OBSERVER> {
+public class WMSLayerCommonSettings<OBSERVER extends IWMSLayerCommonSettingsObserver>
+        extends LayerSettings<OBSERVER> {
+
     /**
-     * Create the WMS layer on startup.
+     * Setting specifying if the WMS layer should be loaded on startup. Default
+     * is false.
      */
-    private boolean useWms; //default = false
-    
+    private boolean useWms;
+
     /**
-     * WMS base query.
+     * Setting specifying the WMS base query.
      */
     private String wmsQuery = "";
+
+    /**
+     * Get the setting that specifies if the WMS layer should be loaded on
+     * startup.
+     * 
+     * @return {@code true} if the WMS layer should be loaded on startup,
+     *         {@code false} if it shouldn't.
+     */
+    public boolean isUseWms() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.useWms;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies if the WMS layer should be loaded on
+     * startup.
+     * 
+     * @param useWms
+     *            {@code true} if the WMS layer should be loaded on startup,
+     *            {@code false} if it shouldn't.
+     */
+    public void setUseWms(final boolean useWms) {
+        try {
+            this.settingLock.writeLock().lock();
+            if(this.useWms == useWms) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.useWms = useWms;
+            for(OBSERVER obs : this.observers) {
+                obs.isUseWmsChanged(useWms);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Gets the setting that specifies the WMS query.
+     * 
+     * @return The WMS query.
+     */
+    public String getWmsQuery() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.wmsQuery;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies the WMS query.
+     * 
+     * @param wmsQuery
+     *            The new WMS query to use.
+     */
+    public void setWmsQuery(String wmsQuery) {
+        try {
+            this.settingLock.writeLock().lock();
+            if(this.wmsQuery.equals(wmsQuery)) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.wmsQuery = wmsQuery;
+            for(OBSERVER obs : this.observers) {
+                obs.wmsQueryChanged(wmsQuery);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
 }
