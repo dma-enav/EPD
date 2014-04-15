b0f4d54c21ec8ca522bf758f71a423b15244214c
Janus Varmarken <varmarken@gmail.com>
2014-04-15 15:06:19 +0200

    EPD-190: Implemented MetocHandlerCommonSettings

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/MetocHandlerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/MetocHandlerCommonSettings.java
@@ -16,20 +16,152 @@
 package dk.dma.epd.common.prototype.settings.handlers;
 
 /**
+ * Maintains settings for a METOC handler.
+ * 
  * @author Janus Varmarken
  */
-public class MetocHandlerCommonSettings extends HandlerSettings<OBSERVER> {
-    
+public class MetocHandlerCommonSettings<OBSERVER extends IMetocHandlerCommonSettingsObserver>
+        extends HandlerSettings<OBSERVER> {
+
     /**
-     * How long should METOC for route be considered valid
+     * How long should METOC for route be considered valid. Unit: minutes.
      */
-    private int metocTtl = 60; // min
+    private int metocTtl = 60;
+
     /**
-     * The minimum interval between metoc polls for active route 0 - never
+     * The minimum interval between METOC polls for active route. 0 means never.
+     * Unit: minutes.
      */
-    private int activeRouteMetocPollInterval = 5; // min
+    private int activeRouteMetocPollInterval = 5;
+
     /**
-     * The tolerance of how long we may drift from plan before METOC is considered invalid 
+     * The tolerance of how long we may drift from plan before METOC is
+     * considered invalid. Unit: minutes.
      */
-    private int metocTimeDiffTolerance = 15; // 15 min
+    private int metocTimeDiffTolerance = 15;
+
+    /**
+     * Get the setting that specifies how long (in minutes) METOC data for a
+     * route is considered valid.
+     * 
+     * @return How long METOC data for a route is considered valid. Unit is
+     *         minutes.
+     */
+    public int getMetocTtl() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.metocTtl;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies how long (in minutes) METOC data for a
+     * route is considered valid.
+     * 
+     * @param metocTtl
+     *            How long METOC data for a route is considered valid. Unit is
+     *            minutes.
+     */
+    public void setMetocTtl(final int metocTtl) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.metocTtl == metocTtl) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify.
+            this.metocTtl = metocTtl;
+            for (OBSERVER obs : this.observers) {
+                obs.metocTtlChanged(metocTtl);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the setting that specifies the minimum interval between METOC polls
+     * for an active route. 0 means never. Unit: minutes.
+     * 
+     * @return The minimum interval between METOC polls for an active route in
+     *         minutes. 0 means never.
+     */
+    public int getActiveRouteMetocPollInterval() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.activeRouteMetocPollInterval;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies the minimum interval between METOC
+     * polls for an active route. 0 means never. Unit: minutes.
+     * 
+     * @param activeRouteMetocPollInterval
+     *            The minimum interval between METOC polls for an active route
+     *            in minutes. 0 means never.
+     */
+    public void setActiveRouteMetocPollInterval(
+            final int activeRouteMetocPollInterval) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.activeRouteMetocPollInterval == activeRouteMetocPollInterval) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.activeRouteMetocPollInterval = activeRouteMetocPollInterval;
+            for (OBSERVER obs : this.observers) {
+                obs.activeRouteMetocPollIntervalChanged(activeRouteMetocPollInterval);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the setting that specifies the tolerance of how long a vessel may
+     * drift from plan before METOC is considered invalid. Unit: minutes.
+     * 
+     * @return The maximum number of minutes a vessel may drift from plan before
+     *         METOC is considered invalid.
+     */
+    public int getMetocTimeDiffTolerance() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.metocTimeDiffTolerance;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies the tolerance of how long a vessel may
+     * drift from plan before METOC is considered invalid. Unit: minutes.
+     * 
+     * @param metocTimeDiffTolerance
+     *            The maximum number of minutes a vessel may drift from plan
+     *            before METOC is considered invalid.
+     */
+    public void setMetocTimeDiffTolerance(final int metocTimeDiffTolerance) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.metocTimeDiffTolerance == metocTimeDiffTolerance) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.metocTimeDiffTolerance = metocTimeDiffTolerance;
+            for (OBSERVER obs : this.observers) {
+                obs.metocTimeDiffToleranceChanged(metocTimeDiffTolerance);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
 }
