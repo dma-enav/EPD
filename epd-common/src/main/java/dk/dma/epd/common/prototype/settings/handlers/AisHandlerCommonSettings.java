15847f24f6115a908f5114b60dc8f2793f990bf6
Janus Varmarken <varmarken@gmail.com>
2014-04-15 18:20:41 +0200

    EPD-190: Implemented AisHandlerCommonSettings.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/AisHandlerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/AisHandlerCommonSettings.java
@@ -15,34 +15,207 @@
  */
 package dk.dma.epd.common.prototype.settings.handlers;
 
+import java.util.Arrays;
+
+import dk.dma.epd.common.prototype.ais.AisHandlerCommon;
+import dk.dma.epd.common.prototype.settings.layers.PastTrackSettings;
+
 /**
+ * Maintains settings for an {@link AisHandlerCommon}. NB: Past track settings
+ * were moved to {@link PastTrackSettings}.
+ * 
  * @author Janus Varmarken
  */
-public class AisHandlerCommonSettings extends HandlerSettings<OBSERVER> {
+public class AisHandlerCommonSettings<OBSERVER extends AisHandlerCommonSettings.IObserver>
+        extends HandlerSettings<OBSERVER> {
+
+    /**
+     * Specifies the prefix to use for a Search and Rescue Target (SART).
+     */
     private int sartPrefix = 970;
-    private String[] simulatedSartMmsi = {}; // Specify comma-separated mmsi
-                                             // list to simulate SarTarget's
-
-    private boolean strict = true; // Strict timeout rules
 
     /**
-     * In minutes.
+     * Comma-separated list of MMSIs that are to be treated as Search and Rescue
+     * Targets (SART).
      */
-    private int pastTrackMaxTime = 4 * 60;
+    private String[] simulatedSartMmsi = {};
 
     /**
-     * In minutes. TODO consider moving to layer settings.
+     * Setting specifying if strict timeout rules are to be used.
      */
-    private int pastTrackDisplayTime = 30;
+    private boolean strict = true;
 
     /**
-     * In meters. TODO move to handler settings.
+     * Gets the setting that specifies the prefix for a Search and Rescue Target
+     * (SART).
+     * 
+     * @return The prefix for a SART.
      */
-    private int pastTrackMinDist = 100;
+    public int getSartPrefix() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.sartPrefix;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
 
     /**
-     * In meters. TODO move to ship specific handler settings or reuse
-     * {@link #pastTrackMinDist} by creating an instance for ownship(handler).
+     * Changes the setting that specifies the prefix for a Search and Rescue
+     * Target (SART).
+     * 
+     * @param sartPrefix
+     *            The new prefix to use for a SART.
      */
-    private int pastTrackOwnShipMinDist = 20;
+    public void setSartPrefix(final int sartPrefix) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.sartPrefix == sartPrefix) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.sartPrefix = sartPrefix;
+            for (OBSERVER obs : this.observers) {
+                obs.sartPrefixChanged(sartPrefix);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the setting that specifies a list of MMSIs that are to be treated as
+     * Search and Rescue Targets (SART).
+     * 
+     * @return A list of MMSIs that are to be treated as SARTs. The returned
+     *         array is a copy of the original data in order to prevent
+     *         reference leaks.
+     */
+    public String[] getSimulatedSartMmsi() {
+        try {
+            this.settingLock.readLock().lock();
+            return Arrays.copyOfRange(this.simulatedSartMmsi, 0,
+                    this.simulatedSartMmsi.length);
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies a list of MMSIs that are to be treated
+     * as Search and Rescue Targets (SART).
+     * 
+     * @param simulatedSartMmsi
+     *            A list of MMSIs that are to be treated as SARTs. This setter
+     *            copies the given array in order to prevent reference leaks.
+     */
+    public void setSimulatedSartMmsi(final String[] simulatedSartMmsi) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (Arrays.equals(this.simulatedSartMmsi, simulatedSartMmsi)) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            // First make a copy to avoid reference leak.
+            String[] copy = Arrays.copyOfRange(simulatedSartMmsi, 0,
+                    simulatedSartMmsi.length);
+            // Update.
+            this.simulatedSartMmsi = copy;
+            for (OBSERVER obs : this.observers) {
+                /*
+                 * We need to make a new copy for each observer, as we are not
+                 * in control of how each individual observer may chose to
+                 * modify the array.
+                 */
+                String[] argCopy = Arrays.copyOfRange(simulatedSartMmsi, 0,
+                        simulatedSartMmsi.length);
+                obs.simulatedSartMmsiChanged(argCopy);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Gets the setting that specifies if strict timeout rules are to be used.
+     * 
+     * @return {@code true} if strict timeout rules are to be used,
+     *         {@code false} otherwise.
+     */
+    public boolean isStrict() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.strict;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies if strict timeout rules are to be
+     * used.
+     * 
+     * @param strict
+     *            {@code true} if strict timeout rules are to be used,
+     *            {@code false} otherwise.
+     */
+    public void setStrict(final boolean strict) {
+        try {
+            this.settingLock.writeLock().lock();
+            if(this.strict == strict) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.strict = strict;
+            for(OBSERVER obs : this.observers) {
+                obs.strictChanged(strict);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Interface for observing an {@link AisHandlerCommonSettings} for changes.
+     * 
+     * @author Janus Varmarken
+     * 
+     */
+    public interface IObserver extends IHandlerSettingsObserver {
+
+        /**
+         * Invoked when {@link AisHandlerCommonSettings#getSartPrefix()} has
+         * changed.
+         * 
+         * @param sartPrefix
+         *            The new SART prefix. See
+         *            {@link AisHandlerCommonSettings#getSartPrefix()} for more
+         *            details.
+         */
+        void sartPrefixChanged(int sartPrefix);
+
+        /**
+         * Invoked when {@link AisHandlerCommonSettings#getSimulatedSartMmsi()}
+         * has changed.
+         * 
+         * @param simulatedSartMmsi
+         *            The new set of simulated SARTs. See
+         *            {@link AisHandlerCommonSettings#getSimulatedSartMmsi()}
+         *            for more details.
+         */
+        void simulatedSartMmsiChanged(String[] simulatedSartMmsi);
+
+        /**
+         * Invoked when {@link AisHandlerCommonSettings#isStrict()} has changed.
+         * 
+         * @param strict
+         *            The new strict value. See
+         *            {@link AisHandlerCommonSettings#isStrict()} for more
+         *            details.
+         */
+        void strictChanged(boolean strict);
+    }
 }
