de4b8f3d4c53b9a36ffbe46d692273d52fc69962
Janus Varmarken <varmarken@gmail.com>
2014-04-16 10:31:33 +0200

    EPD-190: Implemented NetworkSettings.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/network/NetworkSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/network/NetworkSettings.java
@@ -15,6 +15,7 @@
  */
 package dk.dma.epd.common.prototype.settings.network;
 
+import dk.dma.epd.common.prototype.settings.ISettingsObserver;
 import dk.dma.epd.common.prototype.settings.ObservedSettings;
 
 /**
@@ -22,18 +23,18 @@ import dk.dma.epd.common.prototype.settings.ObservedSettings;
  * 
  * @author Janus Varmarken
  */
-public class NetworkSettings<OBSERVER extends INetworkSettingsObserver> extends
-        ObservedSettings<OBSERVER> {
+public class NetworkSettings<OBSERVER extends NetworkSettings.IObserver>
+        extends ObservedSettings<OBSERVER> {
 
     /**
      * Setting specifying a port.
      */
-    private int port;
-    
+    private int port = 9000;
+
     /**
      * Setting specifying a host name or address.
      */
-    private String host;
+    private String host = "localhost";
 
     /**
      * Setting specifying a connection establishment timeout (in milliseconds).
@@ -44,4 +45,197 @@ public class NetworkSettings<OBSERVER extends INetworkSettingsObserver> extends
      * Setting specifying a timeout (in milliseconds) for reading.
      */
     private int readTimeout = 60000;
+
+    /**
+     * Get the port number.
+     * 
+     * @return The port number.
+     */
+    public int getPort() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.port;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the port number.
+     * 
+     * @param port
+     *            The new port number.
+     */
+    public void setPort(final int port) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.port == port) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers
+            this.port = port;
+            for (OBSERVER obs : this.observers) {
+                obs.onPortChanged(port);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the host name.
+     * 
+     * @return The host name.
+     */
+    public String getHost() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.host;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the host name.
+     * 
+     * @param host
+     *            The new host name.
+     */
+    public void setHost(final String host) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.host.equals(host)) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.host = host;
+            for (OBSERVER obs : this.observers) {
+                obs.onHostChanged(host);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the connection establishment timeout. Unit is milliseconds.
+     * 
+     * @return The connection establishment timeout in milliseconds.
+     */
+    public int getConnectTimeout() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.connectTimeout;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the connection establishment timeout. Unit is milliseconds.
+     * 
+     * @param connectTimeout
+     *            The new connection establishment timeout in milliseconds.
+     */
+    public void setConnectTimeout(final int connectTimeout) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.connectTimeout == connectTimeout) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.connectTimeout = connectTimeout;
+            for (OBSERVER obs : this.observers) {
+                obs.onConnectTimeoutChanged(connectTimeout);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the read timeout. Unit is milliseconds.
+     * 
+     * @return The read timeout in milliseconds.
+     */
+    public int getReadTimeout() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.readTimeout;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the read timeout. Unit is milliseconds.
+     * 
+     * @param readTimeout
+     *            The new read timeout in milliseconds.
+     */
+    public void setReadTimeout(final int readTimeout) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.readTimeout == readTimeout) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.readTimeout = readTimeout;
+            for (OBSERVER obs : this.observers) {
+                obs.onReadTimeoutChanged(readTimeout);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Interface for observing a {@link NetworkSettings} for changes.
+     * 
+     * @author Janus Varmarken
+     * 
+     */
+    public interface IObserver extends ISettingsObserver {
+
+        /**
+         * Invoked when {@link NetworkSettings#getPort()} has changed.
+         * 
+         * @param newPort
+         *            The updated port number.
+         */
+        void onPortChanged(int newPort);
+
+        /**
+         * Invoked when {@link NetworkSettings#getHost()} has changed.
+         * 
+         * @param newHost
+         *            The updated host name.
+         */
+        void onHostChanged(String newHost);
+
+        /**
+         * Invoked when {@link NetworkSettings#getConnectTimeout()} has changed.
+         * 
+         * @param newConnectTimeout
+         *            The updated connect timeout. See
+         *            {@link NetworkSettings#getConnectTimeout()} for details
+         *            such as unit.
+         */
+        void onConnectTimeoutChanged(int newConnectTimeout);
+
+        /**
+         * Invoked when {@link NetworkSettings#getReadTimeout()} has changed.
+         * 
+         * @param newReadTimeout
+         *            The updated read timeout. See
+         *            {@link NetworkSettings#getReadTimeout()} for details such
+         *            as unit.
+         */
+        void onReadTimeoutChanged(int newReadTimeout);
+    }
 }
