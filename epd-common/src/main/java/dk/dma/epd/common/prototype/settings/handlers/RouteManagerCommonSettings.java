a79d6f92b050f325c8435326632489e05c9a7f18
Janus Varmarken <varmarken@gmail.com>
2014-04-16 17:25:58 +0200

    EPD-190: Implemented RouteManagerCommonSettings.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/RouteManagerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/handlers/RouteManagerCommonSettings.java
@@ -15,12 +15,188 @@
  */
 package dk.dma.epd.common.prototype.settings.handlers;
 
+import dk.dma.epd.common.prototype.route.RouteManagerCommon;
+
 /**
+ * Maintains settings for a {@link RouteManagerCommon}. Clients may register for
+ * notifications which are fired whenever a setting value has been changed.
+ * 
  * @author Janus Varmarken
  */
-public class RouteManagerCommonSettings extends HandlerSettings<OBSERVER> {
+public class RouteManagerCommonSettings<OBSERVER extends RouteManagerCommonSettings.IObserver>
+        extends HandlerSettings<OBSERVER> {
 
+    /**
+     * Default speed for a (new) route.
+     */
     private double defaultSpeed = 10.0;
+
+    /**
+     * Default turn rad for a (new) route.
+     */
     private double defaultTurnRad = 0.5;
+
+    /**
+     * Default cross track distance for a (new) route.
+     */
     private double defaultXtd = 0.1;
+
+    /**
+     * Get the default speed for a (new) route.
+     * 
+     * @return The default speed for a (new) route. TODO: unit clarification
+     *         needed.
+     */
+    public double getDefaultSpeed() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.defaultSpeed;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the default speed for a (new) route.
+     * 
+     * @param defaultSpeed
+     *            The new default speed for a (new) route. TODO: unit
+     *            clarification needed.
+     */
+    public void setDefaultSpeed(final double defaultSpeed) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.defaultSpeed == defaultSpeed) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.defaultSpeed = defaultSpeed;
+            for (OBSERVER obs : this.observers) {
+                obs.onDefaultSpeedChanged(defaultSpeed);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the default turn rad for a (new) route.
+     * 
+     * @return The default turn rad for a (new) route. TODO: unit clarification
+     *         needed.
+     */
+    public double getDefaultTurnRad() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.defaultTurnRad;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the default turn rad for a (new) route.
+     * 
+     * @param defaultTurnRad
+     *            The new default turn rad for a (new) route. TODO: unit
+     *            clarification needed.
+     */
+    public void setDefaultTurnRad(final double defaultTurnRad) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.defaultTurnRad == defaultTurnRad) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.defaultTurnRad = defaultTurnRad;
+            for (OBSERVER obs : this.observers) {
+                obs.onDefaultTurnRadChanged(defaultTurnRad);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Get the default cross track distance (XTD) for a (new) route.
+     * 
+     * @return The default XTD for a (new) route. TODO: unit clarification
+     *         needed.
+     */
+    public double getDefaultXtd() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.defaultXtd;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Change the default cross track distance (XTD) for a (new) route.
+     * 
+     * @param defaultXtd
+     *            The new default XTD for a (new) route. TODO: unit
+     *            clarification needed.
+     */
+    public void setDefaultXtd(final double defaultXtd) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.defaultXtd == defaultXtd) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.defaultXtd = defaultXtd;
+            for (OBSERVER obs : this.observers) {
+                obs.onDefaultXtdChanged(defaultXtd);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Interface for observing a {@link RouteManagerCommonSettings} for changes.
+     * 
+     * @author Janus Varmarken
+     * 
+     */
+    public interface IObserver extends IHandlerSettingsObserver {
+
+        /**
+         * Invoked when {@link RouteManagerCommonSettings#getDefaultSpeed()} has
+         * changed.
+         * 
+         * @param defaultSpeed
+         *            The new default speed. See
+         *            {@link RouteManagerCommonSettings#getDefaultSpeed()} for
+         *            more details, e.g. unit.
+         */
+        void onDefaultSpeedChanged(double defaultSpeed);
+
+        /**
+         * Invoked when {@link RouteManagerCommonSettings#getDefaultTurnRad()}
+         * has changed.
+         * 
+         * @param defaultTurnRad
+         *            The new default turn rad. See
+         *            {@link RouteManagerCommonSettings#getDefaultTurnRad()} for
+         *            more details, e.g. unit.
+         */
+        void onDefaultTurnRadChanged(double defaultTurnRad);
+
+        /**
+         * Invoked when {@link RouteManagerCommonSettings#getDefaultXtd()} has
+         * changed.
+         * 
+         * @param defaultXtd
+         *            The new default XTD. See
+         *            {@link RouteManagerCommonSettings#getDefaultXtd()} for
+         *            more details, e.g. unit.
+         */
+        void onDefaultXtdChanged(double defaultXtd);
+    }
 }
