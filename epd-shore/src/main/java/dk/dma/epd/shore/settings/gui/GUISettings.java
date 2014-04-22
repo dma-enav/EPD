c793d4e385d60f4ecc4353bfd85c8113c8a8cbd8
Janus Varmarken <varmarken@gmail.com>
2014-04-22 18:31:08 +0200

    EPD-190: Implemented GUISettings (EPD Shore).

--- epd-shore/src/main/java/dk/dma/epd/shore/settings/gui/GUISettings.java
+++ epd-shore/src/main/java/dk/dma/epd/shore/settings/gui/GUISettings.java
@@ -18,11 +18,68 @@ package dk.dma.epd.shore.settings.gui;
 import dk.dma.epd.common.prototype.settings.gui.GUICommonSettings;
 
 /**
+ * Extends {@link GUICommonSettings} with shore specific GUI settings.
+ * 
  * @author Janus Varmarken
  */
-public class GUISettings extends GUICommonSettings {
+public class GUISettings<OBSERVER extends GUISettings.IObserver> extends
+        GUICommonSettings<OBSERVER> {
     /**
      * Points to a file containing the workspace layout.
      */
     private String workspace = "";
+
+    /**
+     * Gets the setting that specifies the path to the workspace file.
+     * 
+     * @return The path to the workspace file.
+     */
+    public String getWorkspace() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.workspace;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Changes the setting that specifies the path to the workspace file.
+     * 
+     * @param workspace
+     *            The new path to the workspace file.
+     */
+    public void setWorkspace(final String workspace) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.workspace.equals(workspace)) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.workspace = workspace;
+            for (OBSERVER obs : this.observers) {
+                obs.workspaceChanged(workspace);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Interface for observing a {@link GUISettings} for changes.
+     * 
+     * @author Janus Varmarken
+     * 
+     */
+    public interface IObserver extends GUICommonSettings.IObserver {
+
+        /**
+         * Invoked when {@link GUISettings#getWorkspace()} has changed.
+         * 
+         * @param workspacePath
+         *            See return value of {@link GUISettings#getWorkspace()}.
+         */
+        void workspaceChanged(String workspacePath);
+    }
 }
