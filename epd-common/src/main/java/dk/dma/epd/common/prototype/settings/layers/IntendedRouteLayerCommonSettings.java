8f8ecc480b9d4d7d8b0bdd051d8e483210e46e65
Janus Varmarken <varmarken@gmail.com>
2014-04-22 15:23:01 +0200

    EPD-190: Made IIntendedRouteLayerCommonSettingsObserver an inner interface of IntendedRouteLayerCommonSettings.

--- epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/IntendedRouteLayerCommonSettings.java
+++ epd-common/src/main/java/dk/dma/epd/common/prototype/settings/layers/IntendedRouteLayerCommonSettings.java
@@ -15,11 +15,14 @@
  */
 package dk.dma.epd.common.prototype.settings.layers;
 
+import dk.dma.epd.common.prototype.layers.intendedroute.IntendedRouteLayerCommon;
 
 /**
+ * Maintains settings for an {@link IntendedRouteLayerCommon}.
+ * 
  * @author Janus Varmarken
  */
-public class IntendedRouteLayerCommonSettings<OBSERVER extends IIntendedRouteLayerCommonSettingsObserver>
+public class IntendedRouteLayerCommonSettings<OBSERVER extends IntendedRouteLayerCommonSettings.IObserver>
         extends LayerSettings<OBSERVER> {
 
     /**
@@ -66,4 +69,23 @@ public class IntendedRouteLayerCommonSettings<OBSERVER extends IIntendedRouteLay
         }
     }
 
+    /**
+     * Interface for observing an {@link IntendedRouteLayerCommonSettings} for
+     * changes.
+     * 
+     * @author Janus Varmarken
+     */
+    public interface IObserver extends LayerSettings.IObserver {
+
+        /**
+         * Invoked when the setting, specifying if intended route filter is
+         * enabled, has been changed.
+         * 
+         * @param useFilter
+         *            {@code true} if intended route filter has been enabled,
+         *            {@code false} if intended route filter has been disabled.
+         */
+        void isIntendedRouteFilterInUseChanged(boolean useFilter);
+
+    }
 }
