698a81279be141a59d9331614f8e9a476a13ac4a
Janus Varmarken <varmarken@gmail.com>
2014-04-22 17:54:08 +0200

    EPD-190: Implemented ENCLayerSettings (EPD Shore).

--- epd-shore/src/main/java/dk/dma/epd/shore/settings/gui/ENCLayerSettings.java
+++ epd-shore/src/main/java/dk/dma/epd/shore/settings/gui/ENCLayerSettings.java
@@ -18,11 +18,76 @@ package dk.dma.epd.shore.settings.gui;
 import dk.dma.epd.common.prototype.settings.layers.ENCLayerCommonSettings;
 
 /**
+ * Maintains shore specific ENC layer settings.
+ * 
  * @author Janus Varmarken
  */
-public class ENCLayerSettings extends ENCLayerCommonSettings {
+public class ENCLayerSettings<OBSERVER extends ENCLayerSettings.IObserver>
+        extends ENCLayerCommonSettings<OBSERVER> {
 
-    //Used internally to check if new map windows should try to make dongle check - if no dongle, don't retry on every new map
+    /**
+     * Used internally to check if new map windows should try to make dongle
+     * check - if no dongle, don't retry on every new map.
+     */
     private boolean encSuccess = true;
-    
+
+    /**
+     * Used internally to check if new map windows should try to make dongle
+     * check - if no dongle, don't retry on every new map.
+     * 
+     * @return {@code true} if a new map window should attempt to make a dongle
+     *         check, {@code false} otherwise.
+     */
+    public boolean isEncSuccess() {
+        try {
+            this.settingLock.readLock().lock();
+            return this.encSuccess;
+        } finally {
+            this.settingLock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Used internally to check if new map windows should try to make dongle
+     * check - if no dongle, don't retry on every new map.
+     * 
+     * @param encSuccess
+     *            {@code true} if a new map window should attempt to make a
+     *            dongle check, {@code false} otherwise.
+     */
+    public void setEncSuccess(final boolean encSuccess) {
+        try {
+            this.settingLock.writeLock().lock();
+            if (this.encSuccess == encSuccess) {
+                // No change, no need to notify observers.
+                return;
+            }
+            // There was a change, update and notify observers.
+            this.encSuccess = encSuccess;
+            for (OBSERVER obs : this.observers) {
+                obs.encSuccessChanged(encSuccess);
+            }
+        } finally {
+            this.settingLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Interface for observing an {@link ENCLayerSettings} for changes.
+     * 
+     * @author Janus Varmarken
+     * 
+     */
+    public interface IObserver extends ENCLayerCommonSettings.IObserver {
+
+        /**
+         * Invoked when {@link ENCLayerSettings#isEncSuccess()} has changed.
+         * 
+         * @param encSuccess
+         *            See return value of
+         *            {@link ENCLayerSettings#isEncSuccess()}.
+         */
+        void encSuccessChanged(boolean encSuccess);
+
+    }
 }
